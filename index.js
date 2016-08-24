1a8ff7ac42753a502dabfe48b093e2aea84f18c2
Dagur Arinbjörn Daníelsson <dagurdan@simnet.s>
2016-08-24 17:35:05 +0000

    mongoose import and index.js organising

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// ========== Package import ==========
 const express = require('express');
 const app = express();
 
+// =========== app setup ===========
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// =========== app startup ===========
 app.listen(process.env.PORT || 8080, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 8080!');
 });
