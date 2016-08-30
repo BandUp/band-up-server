c8b83f42ab79d84b5b891b3c6cb7fc3e516b8c67
Dagur Arinbjörn Daníelsson <dagurdan@simnet.s>
2016-08-30 17:38:08 +0000

    Chat server upp and running

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 // ========== Package import ==========
 const express = require('express');
 const app = express();
-
+const http = require('http').Server(app);
 const port = process.env.PORT || 3000;
+const chat = require('./socket/chatserver');
+chat.setup(http);
 
 // =========== app setup ===========
 app.get('/', (req, res) => {
   res.send('Hello World! and nice to see you');
+});
+
+// get all profiles in range
+app.get('/profiles', (req, res) => {
+  // TODO: current version is a prototype must implement!
+  res.json([
+  		{name: 'Bergþór'},
+  		{name: 'Dagur'},
+  		{name: 'Elvar'},
+  		{name: 'Rafael'}
+  	]);
 });
 
 // =========== app startup ===========
