f7c83935cc35a7c64c0b5ece3eb9db294f8c6642
Dagur Arinbjörn Daníelsson <dagurdan@simnet.s>
2016-09-15 10:49:04 +0000

    minor fix to facebook auth

--- config/facebook-passport.js
+++ config/facebook-passport.js
@@ -1,4 +1,4 @@
-const FacebookStrategy = require('passport-facebook-token').Strategy;
+const FacebookStrategy = require('passport-facebook-token');
 
 // load up user model
 const User = require('../models/user');
