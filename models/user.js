010d30663dce5fc8e3431e7285227d62c605975b
Elvar Laxdal <elvarlax@gmail.com>
2016-09-15 16:21:47 +0000

    Added instruments to database

--- models/user.js
+++ models/user.js
@@ -4,26 +4,27 @@ const bcrypt = require('bcrypt-nodejs');
 
 // schema definition
 let userSchema = mongoose.Schema({
-  local:{
-      username: String,
-      email:    String,
-      password: String,
-      age: Number
+  local: {
+    username: String,
+    email: String,
+    password: String,
+    age: Number
   },
   facebook: {
     id: String,
     token: String,
     name: String,
     email: String
-  }
+  },
+  instrument: [String]
 });
 
 // methoods
-userSchema.methods.generateHash = function(password){
+userSchema.methods.generateHash = function(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
-userSchema.methods.validPassword = function(password){
+userSchema.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.local.password);
 };
 
