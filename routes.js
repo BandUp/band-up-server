722c27baf5c9249b93212e82e4ba66271c33429e
Bergþór Þrastarson <bergthor13@ru.is>
2016-09-26 18:12:24 +0000

    Changing placeholder provider.
    The other one is too slow

--- routes.js
+++ routes.js
@@ -37,10 +37,10 @@ module.exports = function(app, passport){
 
   app.get('/nearby-users', isLoggedIn, (req, res) => {
       res.json([
-        {username: 'Bergþór', instruments:["Piano", "Drums"], genres:["Pop", "Country"], status:"Searching for a band", distance:10, percentage:95, profileImgUrl:"http://lorempixel.com/300/300/"},
-        {username: 'Dagur', instruments:["Guitar", "Bass"], genres:["Rock", "Electronic"], status:"Searching for a band", distance:13, percentage:80, profileImgUrl:"http://lorempixel.com/300/300/"},
-        {username: 'Elvar', instruments:["Vocals", "Percussion"], genres:["Hip Hop", "Jazz"], status:"Looking for a pianist", distance:15, percentage:75, profileImgUrl:"http://lorempixel.com/300/300/"},
-        {username: 'Rafael', instruments:["Harmonica", "Keyboard"], genres:["Indie", "Pop"], status:"Looking for a singer", distance:25, percentage:70, profileImgUrl:"http://lorempixel.com/300/300/"}
+        {username: 'Bergþór', instruments:["Piano", "Drums"], genres:["Pop", "Country"], status:"Searching for a band", distance:10, percentage:95, profileImgUrl:"http://placehold.it/300x300"},
+        {username: 'Dagur', instruments:["Guitar", "Bass"], genres:["Rock", "Electronic"], status:"Searching for a band", distance:13, percentage:80, profileImgUrl:"http://placehold.it/300x300"},
+        {username: 'Elvar', instruments:["Vocals", "Percussion"], genres:["Hip Hop", "Jazz"], status:"Looking for a pianist", distance:15, percentage:75, profileImgUrl:"http://placehold.it/300x300"},
+        {username: 'Rafael', instruments:["Harmonica", "Keyboard"], genres:["Indie", "Pop"], status:"Looking for a singer", distance:25, percentage:70, profileImgUrl:"http://placehold.it/300x300"}
       ]);
   });
 
