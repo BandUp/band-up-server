88529e6832147eaf4ec55d34c75aa5bf4a8861f9
Elvar Laxdal <elvarlax@gmail.com>
2016-10-13 23:09:21 +0000

    Fixing google-passport.js

--- config/google-passport.js
+++ config/google-passport.js
@@ -1,12 +1,13 @@
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GoogleStrategy = require('passport-google-token').Strategy;
+
+// load up user model
 const User = require('../models/user');
-const GoogleTokenStrategy = require('passport-google-token').Strategy;
 
 module.exports = function(passport){
   passport.use(new GoogleTokenStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  }, (accessToken, refreshToken, profile, done) =>{
+    clientID: process.env.GOOGLE_CLIENT,
+    clientSecret: process.env.GOOGLE_SECRET
+  }, (token, refreshToken, profile, done) => {
     process.nextTick(() => {
       User.findOne({'google.id': profile.id}, (err, user) => {
         if(err) return done(err);
@@ -16,7 +17,7 @@ module.exports = function(passport){
           let newUser = new User();
           newUser.google.id = profile.id;
           newUser.google.token = token;
-          newUser.username     = profile.displayName;
+          newUser.username = profile.displayName;
           newUser.email = profile.emails[0].value;
 
           newUser.save((err) => {
@@ -29,39 +30,4 @@ module.exports = function(passport){
       });
     });
   }));
-};
-
-
-module.exports = function(passport) {
-  passport.use(new GoogleStrategy({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    }, (token, refreshToken, profile, done) => {
-          // asynchronous
-          process.nextTick(() => {
-            // look for pre-existing account
-            User.findOne({'google.id': profile.id}, (err, user) => {
-              if(err) return done(err);
-
-              // if user is found log them in
-              if(user){
-                 return done(null, user);
-               }
-               else {
-                 let newUser = new User();
-                 newUser.google.id = profile.id;
-                 newUser.google.token = token;
-                 newUser.username = profile.displayName;
-                 newUser.email = profile.emails[0].value;
-
-                 newUser.save((err) => {
-                   if(err) throw err;
-
-                   // if succesful return the new user
-                   return done(null, newUser);
-                 });
-               }
-            });
-          });
-        }));
 };
