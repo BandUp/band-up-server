0ae73a8b0ecf40379eb32aa57807b35b8070cee4
Bergþór Þrastarson <bergthor13@ru.is>
2016-10-14 01:43:12 +0000

    testing google login

--- config/google-passport.js
+++ config/google-passport.js
@@ -1,4 +1,3 @@
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const User = require('../models/user');
 const GoogleTokenStrategy = require('passport-google-token').Strategy;
 
@@ -6,13 +5,19 @@ module.exports = function(passport){
   passport.use(new GoogleTokenStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  }, (accessToken, refreshToken, profile, done) =>{
+  }, (accessToken, refreshToken, profile, done) => {
     process.nextTick(() => {
+    	console.log("HAHAH");
       User.findOne({'google.id': profile.id}, (err, user) => {
-        if(err) return done(err);
+        console.log("AHAH");
+        if(err) {
+        	console.log("ERROR");
+        	return done(err);
+        }
         if(user){
+        	console.log("USER");
           return done(null, user);
-        }else{
+        } else {
           let newUser = new User();
           newUser.google.id = profile.id;
           newUser.google.token = token;
@@ -32,36 +37,36 @@ module.exports = function(passport){
 };
 
 
-module.exports = function(passport) {
-  passport.use(new GoogleStrategy({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    }, (token, refreshToken, profile, done) => {
-          // asynchronous
-          process.nextTick(() => {
-            // look for pre-existing account
-            User.findOne({'google.id': profile.id}, (err, user) => {
-              if(err) return done(err);
+// module.exports = function(passport) {
+//   passport.use(new GoogleStrategy({
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//     }, (token, refreshToken, profile, done) => {
+//           // asynchronous
+//           process.nextTick(() => {
+//             // look for pre-existing account
+//             User.findOne({'google.id': profile.id}, (err, user) => {
+//               if(err) return done(err);
 
-              // if user is found log them in
-              if(user){
-                 return done(null, user);
-               }
-               else {
-                 let newUser = new User();
-                 newUser.google.id = profile.id;
-                 newUser.google.token = token;
-                 newUser.username = profile.displayName;
-                 newUser.email = profile.emails[0].value;
+//               // if user is found log them in
+//               if(user){
+//                  return done(null, user);
+//                }
+//                else {
+//                  let newUser = new User();
+//                  newUser.google.id = profile.id;
+//                  newUser.google.token = token;
+//                  newUser.username = profile.displayName;
+//                  newUser.email = profile.emails[0].value;
 
-                 newUser.save((err) => {
-                   if(err) throw err;
+//                  newUser.save((err) => {
+//                    if(err) throw err;
 
-                   // if succesful return the new user
-                   return done(null, newUser);
-                 });
-               }
-            });
-          });
-        }));
-};
+//                    // if succesful return the new user
+//                    return done(null, newUser);
+//                  });
+//                }
+//             });
+//           });
+//         }));
+// };
