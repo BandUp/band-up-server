1379f2fac317d25ce3a786bab3271139962c9086
Elvar Laxdal <elvarlax@gmail.com>
2016-10-23 23:56:55 +0000

    Removing duplicates

--- config/gcmSender.js
+++ config/gcmSender.js
@@ -26,26 +26,6 @@ class gcmSender {
     });
   }
 
-  sendMsgNotification(senderid, recieverid, msg) {
-    User.findOne({_id: recieverid}, (err, doc) => {
-      let message = new gcm.Message({
-        data: {
-          from: senderid
-        },
-        notification: {
-          title: "you have a new message from someone",
-          icon: "ic_launcher",
-          body: msg
-        }
-      });
-
-      this.sender.send(message, {registrationToken: doc.gcmToken}, (err, response) => {
-        if(err) console.log(err);
-        else    console.log(response);
-      });
-    });
-  }
-
   sendTestMessage(regTokens){
     let message = new gcm.Message({
       data: {key1: "Hello world!"},
