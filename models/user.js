dbccfc3086e3e4d943ff598139c0c93a956d69fd
Elvar Laxdal <elvarlax@gmail.com>
2016-10-30 01:39:40 +0000

    Adding date for age

--- models/user.js
+++ models/user.js
@@ -1,119 +1,115 @@
 // load things we need
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt-nodejs');
-const Instrument = require('./instrument');
-const Genres = require('./genre');
+const instrument = require('./instrument');
+const genres = require('./genre');
 
 // schema definition
 let userSchema = mongoose.Schema({
-    username: {
-        type: String,
-        default: ""
-    },
-    email: {
-        type: String,
-        default: ""
-    },
-    age: {
-        type: Number,
-        default: 0
-    },
-    local: {
-        password: String,
-        age: Number
-    },
-    facebook: {
-        id: String,
-        token: String
-    },
-    google: {
-        id: String
-    },
-    soundcloud: {
-        id: String,
-        token: String
-    },
-    aboutme: {
-        type: String,
-        default: ""
-    },
-    searchradius: {
-        type: Number,
-        default: 5
-    },
-    instruments: [String],
-    genres: [String],
-    hasFinishedSetup: {
-        type: Boolean,
-        default: false
-    },
-    location: {
-        lat: Number,
-        lon: Number,
-        valid: Boolean
-    },
-    liked: {
-        type: [String],
-        default: []
-    },
-    matched: {
-        type: [String],
-        default: []
-    },
-    image: {
-        url: String,
-        public_id: String
-    },
-    gcmToken: {
-        type: String,
-        default: ""
-    }
+	username: {
+		type: String,
+		default: ""
+	},
+	email: {
+		type: String,
+		default: ""
+	},
+	age: {
+		type: Date
+	},
+	local: {
+		password: String
+	},
+	facebook: {
+		id: String,
+		token: String
+	},
+	google: {
+		id: String
+	},
+	soundcloud: {
+		id: String,
+		token: String
+	},
+	aboutme: {
+		type: String,
+		default: ""
+	},
+	searchradius: {
+		type: Number,
+		default: 5
+	},
+	instruments: [String],
+	genres: [String],
+	hasFinishedSetup: {
+		type: Boolean,
+		default: false
+	},
+	location: {
+		lat: Number,
+		lon: Number,
+		valid: Boolean
+	},
+	liked: {
+		type: [String],
+		default: []
+	},
+	matched: {
+		type: [String],
+		default: []
+	},
+	image: {
+		url: String,
+		public_id: String
+	},
+	gcmToken: {
+		type: String,
+		default: ""
+	}
 }, {
-    toObject: {
-        transform: function(doc, ret) {
-            delete ret.facebook;
-            delete ret.google;
-            delete ret.soundcloud;
-            delete ret.local;
-        }
-    },
-    toJSON: {
-        transform: function(doc, ret) {
-            delete ret.facebook;
-            delete ret.google;
-            delete ret.soundcloud;
-            delete ret.local;
-            // fetch instr list
-            Instrument.find({
-                    '_id': {
-                        $in: ret.instruments
-                    }
-                }).exec()
-                .then((instrumentList) => {
-                    ret.instruments = instrumentList;
-                });
-            Genres.find({
-                    '_id': {
-                        $in: ret.genres
-                    }
-                }).exec()
-                .then((genreList) => {
-                    ret.genres = genreList;
-                });
+	toObject: {
+		transform: function(doc, ret) {
+			delete ret.facebook;
+			delete ret.google;
+			delete ret.soundcloud;
+			delete ret.local;
+		}
+	},
+	toJSON: {
+		transform: function(doc, ret) {
+			delete ret.facebook;
+			delete ret.google;
+			delete ret.soundcloud;
+			delete ret.local;
 
-            ret.instruments = ["asdf"];
-            return ret;
-        }
-    }
+			instrument.find({
+					'_id': {
+						$in: ret.instruments
+					}
+				}).exec()
+				.then((instrumentList) => {
+					ret.instruments = instrumentList;
+				});
+
+			genres.find({
+					'_id': {
+						$in: ret.genres
+					}
+				}).exec()
+				.then((genreList) => {
+					ret.genres = genreList;
+				});
+		}
+	}
 });
 
 // methoods
 userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+	return bcrypt.compareSync(password, this.local.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
--- routes/shared.js
+++ routes/shared.js
@@ -1,77 +1,78 @@
 const geolib = require('geolib');
 
 module.exports = {
-    itemNamesToMap: function(item, callback) {
-        item.find({}, (err, itemDoc) => {
-            if (err) {
-                console.log("Error occurred:");
-                console.log(err);
-                res.status(500).send("Unknown internal server error occurred.");
-                callback(null);
-                return;
-            }
-            let itemMap = {};
+	itemNamesToMap: function(item, callback) {
+		item.find({}, (err, itemDoc) => {
+			if (err) {
+				console.log("Error occurred:");
+				console.log(err);
+				res.status(500).send("Unknown internal server error occurred.");
+				callback(null);
+				return;
+			}
+			let itemMap = {};
 
-            for (let i = 0; i < itemDoc.length; i++) {
-                itemMap[itemDoc[i]._id] = itemDoc[i].name;
-            }
-            callback(itemMap);
-        });
-    },
-    // currUser: The user that is currently logged in.
-    // targUser: The user that is currently being worked on.
-    // instruMap: A map with key-value pairs of IDs and names of instruments.
-    // genresMap: A map with key-value pairs of IDs and names of genres.
-    userToDTO: function(currUser, targUser, instruMap, genresMap) {
-        //console.log(currUser);
-        //console.log(targUser);
-        let distanceToUser;
-        if (currUser.location.valid && targUser.location.valid) {
-            distanceToUser = geolib.getDistance({
-                latitude: currUser.location.lat,
-                longitude: currUser.location.lon
-            }, {
-                latitude: targUser.location.lat,
-                longitude: targUser.location.lon
-            });
-            distanceToUser /= 1000;
+			for (let i = 0; i < itemDoc.length; i++) {
+				itemMap[itemDoc[i]._id] = itemDoc[i].name;
+			}
+			callback(itemMap);
+		});
+	},
+	// currUser: The user that is currently logged in.
+	// targUser: The user that is currently being worked on.
+	// instruMap: A map with key-value pairs of IDs and names of instruments.
+	// genresMap: A map with key-value pairs of IDs and names of genres.
+	userToDTO: function(currUser, targUser, instruMap, genresMap) {
+		//console.log(currUser);
+		//console.log(targUser);
+		let distanceToUser;
+		if (currUser.location.valid && targUser.location.valid) {
+			distanceToUser = geolib.getDistance({
+				latitude: currUser.location.lat,
+				longitude: currUser.location.lon
+			}, {
+				latitude: targUser.location.lat,
+				longitude: targUser.location.lon
+			});
+			distanceToUser /= 1000;
 
-        } else {
-            distanceToUser = null;
-        }
+		} else {
+			distanceToUser = null;
+		}
 
-        function makeFilter(user) {
-            return function(val) {
-                return user.genres.indexOf(val) !== -1;
-            };
-        }
+		function makeFilter(user) {
+			return function(val) {
+				return user.genres.indexOf(val) !== -1;
+			};
+		}
 
-        // get total number of genres for user with longer list
-        let numGenres = Math.max(currUser.genres.length, targUser.genres.length);
+		// get total number of genres for user with longer list
+		let numGenres = Math.max(currUser.genres.length, targUser.genres.length);
 
-        // filter call returns list shared genres
-        let perc = (currUser.genres.filter(makeFilter(targUser)).length / numGenres) * 100;
+		// filter call returns list shared genres
+		let perc = (currUser.genres.filter(makeFilter(targUser)).length / numGenres) * 100;
 
 
-        let userDTO = {
-            _id: targUser._id,
-            username: targUser.username,
-            status: "Not Implemented",
-            instruments: [],
-            genres: [],
-            distance: distanceToUser,
-            percentage: perc,
-            image: targUser.image,
-            age: 100,
-            aboutme: targUser.aboutme
-        };
-        for (let j = 0; j < targUser.instruments.length; j++) {
-            userDTO.instruments.push(instruMap[targUser.instruments[j]]);
-        }
+		let userDTO = {
+			_id: targUser._id,
+			username: targUser.username,
+			status: "Not Implemented",
+			instruments: [],
+			genres: [],
+			distance: distanceToUser,
+			percentage: perc,
+			image: targUser.image,
+			age: targUser.age,
+			aboutme: targUser.aboutme
+		};
 
-        for (let j = 0; j < targUser.genres.length; j++) {
-            userDTO.genres.push(genresMap[targUser.genres[j]]);
-        }
-        return userDTO;
-    }
+		for (let i = 0; i < targUser.instruments.length; i++) {
+			userDTO.instruments.push(instruMap[targUser.instruments[i]]);
+		}
+
+		for (let i = 0; i < targUser.genres.length; i++) {
+			userDTO.genres.push(genresMap[targUser.genres[i]]);
+		}
+		return userDTO;
+	}
 };
