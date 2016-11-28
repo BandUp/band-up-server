a916599069344f430537ef1030f0328181abe000
Dagur Arinbjörn Daníelsson <dagur13@ru,is>
2016-11-28 09:47:01 +0000

    test of like route (case 1 user can like another user)

--- test/like-match-routes-test.js
+++ test/like-match-routes-test.js
@@ -6,7 +6,23 @@ const request = require('supertest');
 const clearDB = require('mocha-mongoose')(dbURI);
 const user = require('../models/user');
 
+let user1;
+let user2;
+
 module.exports = function(app) {
+    // this runs before tests
+    before((done) => {
+        if (mongoose.connection.db) return done();
+        mongoose.connect(dbURI, done);
+    });
+
+    // clean out database before every test
+    beforeEach(function(done) {
+        clearDB((err) => {
+            if (err) throw err;
+            done();
+        });
+    });
 
     describe('User like', function() {
         describe('#like', function() {
@@ -19,7 +35,16 @@ module.exports = function(app) {
                 newUser.dateOfBirth = "Sun Nov 18 1990 15:20:58 GMT+0000 (Greenwich Standard Time)";
                 newUser.save((err) => {
                     if (err) throw err;
-                    done();
+                    user1 = newUser;
+                    let newUser2 = new user();
+                    newUser2.username = "TestPerson2";
+                    newUser2.local.password = newUser2.generateHash("SecretTestPassword2");
+                    newUser2.email = "test@test.is";
+                    newUser2.dateOfBirth = "Sun Nov 18 1990 15:20:58 GMT+0000 (Greenwich Standard Time)";
+                    newUser2.save((err) => {
+                        user2 = newUser2;
+                        done();
+                    });
                 });
             });
 
@@ -27,7 +52,7 @@ module.exports = function(app) {
              * Author: Elvar
              * Signees:
              */
-            it('user1 should like user2', function(done) {
+            it('user1 should be able to like like user2', function(done) {
                 let agent = request.agent(app);
                 agent
                     .post('/login-local')
@@ -37,7 +62,19 @@ module.exports = function(app) {
                     }).end((err, res) => {
                         if (err) throw err;
                         /* Work in progress */
-                        done();
+                        agent
+                            .post('/like')
+                            .send({
+                                userID: user2._id
+                            }).end((err, res) => {
+                                if(err) throw err;
+                                user.findById(user1._id, (err, doc) => {
+                                    if(err) throw err;
+                                    doc.liked.should.containEql(user2._id.toString());
+                                    res.body.isMatch.should.be.false();
+                                    done();
+                                });
+                            });
                     });
             });
         });
