dd4311761e8f23f3f377e27aac406281b4eb7c4b
Bergþór Þrastarson <bergthor13@ru.is>
2017-01-05 19:31:34 +0000

    Removing logout

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -98,7 +98,6 @@ module.exports = function(app, passport) {
 	});
 
 	app.delete("/user-delete", isLoggedIn, (req, res) => {
-		req.logout();
 		User.remove({ "_id": req.user._id }, (err) => {
 			if (err) throw err;
 			res.status(204).json({}).send();
